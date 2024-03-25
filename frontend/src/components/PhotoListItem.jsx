694ca22a8be2aac40588f89ccb2bb36fd13a0fc0
evanb <labber@Evangelos>
2024-03-25 12:13:31 -0400

    CSS cleanup

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,19 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-  const PhotoListItem = ({ id, location, imageSource, username, profile }) => {
-    return (
-      <div className="photo-list-item" key={id}>
-        <img src={imageSource} alt={`Photo by ${username}`} />
-        <div className="photo-info">
-          <img src={profile} alt={`${username}'s profile`} />
-          <div className="photo-details">
-            <h4>{username}</h4>
-            <p>{location}</p>
-          </div>
+const PhotoListItem = ({ id, location, imageSource, username, profile }) => {
+  return (
+    <div className="photo-list__item" key={id}>
+      <img src={imageSource} alt={`Photo by ${username}`} className="photo-list__image" />
+      <div className="photo-list__user-details">
+        <img src={profile} alt={`${username}'s profile`} className="photo-list__user-profile" />
+        <div className="photo-list__user-info">
+          <h4 className="photo-list__user-name">{username}</h4>
+          <p className="photo-list__user-location">{`${location.city}, ${location.country}`}</p>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default PhotoListItem;
