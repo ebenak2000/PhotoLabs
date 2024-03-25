09ff115f61fe78d0804fb4a37222b5445f010bb8
evanb <labber@Evangelos>
2024-03-25 12:31:38 -0400

    Frontend Cleanup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,28 +3,43 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
+const photosArray = [
+  {
+    id: "1",
+    location: { city: "Montreal", country: "Canada" },
+    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+    username: "Joe Example",
+    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
   },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+  {
+    id: "2",
+    location: { city: "Paris", country: "France" },
+    imageSource: `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`,
+    username: "Jane Doe",
+    profile: `${process.env.PUBLIC_URL}/profile-2.jpg`,
+  },
+  {
+    id: "3",
+    location: { city: "Tokyo", country: "Japan" },
+    imageSource: `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`,
+    username: "John Smith",
+    profile: `${process.env.PUBLIC_URL}/profile-3.jpg`,
+  }
+];
 
-const App = () => {
+const App = () => (
   <div className="App">
-    <PhotoListItem
-      id={sampleDataForPhotoListItem.id}
-      location={`${sampleDataForPhotoListItem.location.city}, ${sampleDataForPhotoListItem.location.country}`}
-      imageSource={sampleDataForPhotoListItem.imageSource}
-      username={sampleDataForPhotoListItem.username}
-      profile={sampleDataForPhotoListItem.profile}
-    />  
+    {photosArray.map(photo => (
+      <PhotoListItem
+        key={photo.id}
+        id={photo.id}
+        location={`${photo.location.city}, ${photo.location.country}`}
+        imageSource={photo.imageSource}
+        username={photo.username}
+        profile={photo.profile}
+      />
+    ))}
   </div>
-
-};
+);
 
 export default App;
