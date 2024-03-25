c99b96cc35e4d22ea92997f54090225ec1a43928
evanb <labber@Evangelos>
2024-03-25 11:12:57 -0400

    PhotoList Cleanup

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,19 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
-};
+  const PhotoListItem = ({ id, location, imageSource, username, profile }) => {
+    return (
+      <div className="photo-list-item" key={id}>
+        <img src={imageSource} alt={`Photo by ${username}`} />
+        <div className="photo-info">
+          <img src={profile} alt={`${username}'s profile`} />
+          <div className="photo-details">
+            <h4>{username}</h4>
+            <p>{location}</p>
+          </div>
+        </div>
+      </div>
+    );
+  };
 
 export default PhotoListItem;
