cce6d18cb2d89fbed001398b1ebd4c9ad7095398
evanb <labber@Evangelos>
2024-03-26 17:45:12 -0400

    troubleshooting

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,65 +1,11 @@
 import React from "react";
-
-import "../styles/PhotoList.scss";
 import PhotoListItem from './PhotoListItem';
-const sampleDataForPhotoList = [
-  {
-    id: "1",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-1-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    },
-    user: {
-      id: "1",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-  },
-  {
-    id: "2",
-    location: {
-      city: "Toronto",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-2-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`,
-    },
-    user: {
-      id: "2",
-      username: "exampleuser",
-      name: "Mary Smith",
-      profile: `${process.env.PUBLIC_URL}/profile-2.jpg`,
-    },
-  },
-  {
-    id: "3",
-    location: {
-      city: "Ottawa",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-3-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`,
-    },
-    user: {
-      id: "3",
-      username: "exampleuser",
-      name: "John Doe",
-      profile: `${process.env.PUBLIC_URL}/profile-3.jpg`,
-    },
-  },
-];
+import "../styles/PhotoList.scss";
 
-const PhotoList = () => {
+const PhotoList = ({ photos }) => {
   return (
     <ul className="photo-list">
-      {sampleDataForPhotoList.map(photo => (
+      {photos.map(photo => (
         <li key={photo.id}>
           <PhotoListItem
             id={photo.id}
--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,32 +2,11 @@ import React from "react";
 import TopicListItem from './TopicListItem';
 import "../styles/TopicList.scss";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
-
-const TopicList = () => {
+const TopicList = ({ topics }) => {
   return (
     <div className="top-nav-bar__topic-list">
-      {sampleDataForTopicList.map(topic => (
-        <TopicListItem
-          key={topic.id}
-          label={topic.title}
-        />
+      {topics.map(topic => (
+        <TopicListItem key={topic.id} label={topic.title} />
       ))}
     </div>
   );
