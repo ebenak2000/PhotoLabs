64bdbfa4881486694d50af20c7fc85a37db7e1a6
evanb <labber@Evangelos>
2024-04-11 12:59:41 -0400

    final touches

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { useState } from 'react';
 import PhotoListItem from './PhotoListItem';
+import PhotoDetailsModal from '../routes/PhotoDetailsModal';
 import "../styles/PhotoList.scss";
 import photos from '../mocks/photos';
 
 const PhotoList = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
+
+  const handlePhotoClick = (photo) => {
+    setSelectedPhoto(photo);
+    setIsModalVisible(true);
+  };
+
   return (
-    <ul className="photo-list">
-      {photos.map(photo => (
-        <li key={photo.id}>
-          <PhotoListItem
-            id={photo.id}
-            location={`${photo.location.city}, ${photo.location.country}`}
-            imageSource={photo.urls.regular}
-            username={photo.user.name}
-            profile={photo.user.profile}
-          />
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="photo-list">
+        {photos.map(photo => (
+          <li key={photo.id} onClick={() => handlePhotoClick(photo)}>
+            <PhotoListItem
+              id={photo.id}
+              location={`${photo.location.city}, ${photo.location.country}`}
+              imageSource={photo.urls.regular}
+              username={photo.user.name}
+              profile={photo.user.profile}
+            />
+          </li>
+        ))}
+      </ul>
+      <PhotoDetailsModal isVisible={isModalVisible} setIsModalVisible={setIsModalVisible}>
+        {selectedPhoto && (
+          <div>
+            <img src={selectedPhoto.urls.regular} alt={`Photo by ${selectedPhoto.user.name}`} style={{ maxWidth: '100%' }} />
+            <p>Location: {`${selectedPhoto.location.city}, ${selectedPhoto.location.country}`}</p>
+            <p>Photographer: {selectedPhoto.user.name}</p>
+          </div>
+        )}
+      </PhotoDetailsModal>
+    </>
   );
 };
 
--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-
-import '../styles/PhotoDetailsModal.scss'
+import '../styles/PhotoDetailsModal.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = ({ isVisible, children, setIsModalVisible }) => {
+  if (!isVisible) return null;
+
+  const handleCloseClick = () => {
+    setIsModalVisible(false);
+  };
+
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
-        <img src={closeSymbol} alt="close symbol" />
+      <div className="photo-details-modal__content">
+        {children}
+      </div>
+      <button className="photo-details-modal__close-button" onClick={handleCloseClick}>
+        <img src={closeSymbol} alt="Close" />
       </button>
     </div>
-  )
+  );
 };
 
 export default PhotoDetailsModal;
