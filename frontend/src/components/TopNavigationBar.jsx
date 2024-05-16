14d26ff0d7f5c3c38d956d273e8e3ff798ae585c
evanb <labber@Evangelos>
2024-05-15 23:43:15 -0400

    final changes

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { useFavorites } from '../contexts/favoritesContext';
-import TopicList from './TopicList';
 import FavBadge from './FavBadge';
 import '../styles/TopNavigationBar.scss';
 
-const TopNavigationBar = () => {
-  const { favorites } = useFavorites();
-
+const TopNavigationBar = ({ topics, favourites }) => {
   return (
-    <div className="top-nav-bar">
-      <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList />
-      <FavBadge count={favorites.length} />
+    <div className="top-navigation-bar">
+      <div className="logo">PhotoLabs</div>
+      <ul className="topics-list">
+        {topics.map(topic => (
+          <li key={topic.id} className="topic-item">
+            {topic.title}
+          </li>
+        ))}
+      </ul>
+      <FavBadge count={favourites.length} />
     </div>
   );
 };
--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,16 +1,53 @@
-import React from 'react';
-import TopNavigationBar from '../components/TopNavigationBar';
-import PhotoList from '../components/PhotoList';
+import React, { useEffect, useState } from 'react';
+import PhotoList from './PhotoList';
+import TopicList from './TopicList';
+import TopNavigationBar from './TopNavigationBar';
 import '../styles/HomeRoute.scss';
 
-const HomeRoute = ({ photos, topics }) => {
+const HomeRoute = () => {
+  const [photos, setPhotos] = useState([]);
+  const [topics, setTopics] = useState([]);
+  const [selectedTopic, setSelectedTopic] = useState(null);
+  const [favourites, setFavourites] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const photosResponse = await fetch('http://localhost:8001/api/photos');
+      const photosData = await photosResponse.json();
+      setPhotos(photosData);
+
+      const topicsResponse = await fetch('http://localhost:8001/api/topics');
+      const topicsData = await topicsResponse.json();
+      setTopics(topicsData);
+    };
+    fetchData();
+  }, []);
+
+  const handleTopicClick = (topicId) => {
+    setSelectedTopic(topicId);
+  };
+
+  const toggleFavourite = (photoId) => {
+    setFavourites((prevFavourites) => {
+      if (prevFavourites.includes(photoId)) {
+        return prevFavourites.filter(id => id !== photoId);
+      } else {
+        return [...prevFavourites, photoId];
+      }
+    });
+  };
+
+  const filteredPhotos = selectedTopic
+    ? photos.filter(photo => photo.topics.some(topic => topic.id === selectedTopic))
+    : photos;
+
   return (
     <div className="home-route">
-      <TopNavigationBar topics={topics} />
-      <div className="home-route__photos-section">
-        <PhotoList photos={photos} />
-      </div>
+      <TopNavigationBar topics={topics} favourites={favourites} />
+      <TopicList topics={topics} onTopicClick={handleTopicClick} />
+      <PhotoList photos={filteredPhotos} onToggleFavourite={toggleFavourite} favourites={favourites} />
     </div>
   );
 };
+
 export default HomeRoute;
